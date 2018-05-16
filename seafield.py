fbbb66c86254e132b247103811cf8d219776d82c
lisfer <makrushinds@gmail.com>
2018-05-16 14:35:48 +0300

    added Cell class

--- seafield.py
+++ seafield.py
@@ -1,13 +1,22 @@
-class CellValues:
+
+class Cell:
     EMPTY = 0
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.value = Cell.EMPTY
 
 
 class SeaField:
 
-    def __init__(self, max_x, max_y, value=CellValues.EMPTY):
+    HORIZONTAL = 0
+    VERTICAL = 1
+
+    def __init__(self, max_x, max_y):
         self.max_x = max_x
         self.max_y = max_y
-        self._field = [[value for x in range(max_x)] for y in range(max_y)]
+        self._field = [[Cell(x, y) for x in range(max_x)] for y in range(max_y)]
 
     def mark_cell(self, coord_x, coord_y, value):
-        self._field[coord_x][coord_y] = value
+        self._field[coord_x][coord_y].value = value
--- test_seafield.py
+++ test_seafield.py
@@ -1,15 +1,19 @@
 import unittest
-from seafield import SeaField
+from seafield import SeaField, Cell
 
 
 class SeaFieldTest(unittest.TestCase):
 
     def test_mark_cell(self):
-        ff = SeaField(4, 4)
+        ff = SeaField(5, 5)
         ff.mark_cell(2, 2, 10)
         for row_i, row in enumerate(ff._field):
             for cell_i, cell in enumerate(row):
                 if cell_i == 2 and row_i == 2:
-                    self.assertEqual(cell, 10)
+                    self.assertEqual(cell.value, 10)
                 else:
-                    self.assertEqual(cell, 0)
+                    self.assertEqual(cell.value, Cell.EMPTY)
+
+    def test_set_ship(self):
+        ff = SeaField(5, 5)
+        # ff.set_ship(0, 2, 4, SeaField.HORIZONTAL)
