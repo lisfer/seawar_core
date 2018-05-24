115c456a4e0d5ee86eda6223b4d416fe83fb3337
lisfer <makrushinds@gmail.com>
2018-05-24 16:11:26 +0300

    refactoring: added cell generator

--- seaplayground.py
+++ seaplayground.py
@@ -1,121 +1,57 @@
+from functools import partial
 from itertools import chain
 
 
-class Coord:
+class Cell:
+
+    EMPTY = 0
+    SHIP = 10
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
-
-class Cell(Coord):
-    BORDER = 1
-    SHIP = 10
-    EMPTY = 0
-
-    def __init__(self, x, y):
-        super(Cell, self).__init__(x, y)
         self.value = Cell.EMPTY
 
-
-class ShipCouldNotBePlaced(Exception):
-    pass
-
-
-class OutOfRange(Exception):
-    pass
+    def __str__(self):
+        return f'<Cell: ({self.x}; {self.y} = {self.value})>'
 
 
-class SeaField:
-
-    HORIZONTAL = 0
-    VERTICAL = 1
+class Field:
 
     def __init__(self, max_x, max_y):
-        self.max_x = max_x
-        self.max_y = max_y
-        self._field = [[Cell(x, y) for x in range(max_x)] for y in range(max_y)]
+        self.max_x = max_x,
+        self.may_y = max_y
+        self._field = [[Cell(i, j) for i in range(max_x)] for j in range(max_y)]
+        self._cells = list(chain(*self._field))
 
-    def mark_cell(self, coord_x, coord_y, value):
-        if self._correct_coord(coord_x, coord_y):
-            self._field[coord_y][coord_x].value = value
-            return True
-        else:
-            return False
+    def set(self, coord_x, coord_y, value):
+        self._field[coord_y][coord_x].value = value
 
-    def _draw_ship(self, coord_x, coord_y, ship_length, direction=None):
-        for cell in self._generate_cells(coord_x, coord_y, ship_length, direction):
-            cell.value = Cell.SHIP
-        return True
-
-    def _correct_coord(self, coord_x, coord_y):
-        return 0 <= coord_y < self.max_y and 0 <= coord_x < self.max_x
-
-    def _get_border_corners(self, coord_x, coord_y, ship_length, direction):
-
-        if direction == SeaField.VERTICAL:
-            return [(coord_x - 1, coord_y - 1, ship_length + 2, direction),
-                    (coord_x + 1, coord_y - 1, ship_length + 2, direction),
-                    (coord_x, coord_y - 1, 1, None),
-                    (coord_x, coord_y + ship_length, 1, None)]
-        else:
-            return [(coord_x - 1, coord_y - 1, ship_length + 2, direction),
-                    (coord_x - 1, coord_y + 1, ship_length + 2, direction),
-                    (coord_x - 1, coord_y, 1, None),
-                    (coord_x + ship_length, coord_y, 1, None)]
-
-    def _draw_border(self, coord_x, coord_y, ship_length, direction=None):
-        cell_generators = [
-            self._generate_cells(*row, strict=False)
-            for row in self._get_border_corners(coord_x, coord_y, ship_length, direction)]
-
-        for cell in chain(*cell_generators):
-            cell.value = Cell.BORDER
-
-    def set_ship(self, coord_x, coord_y, ship_length, direction=None):
-        self._draw_ship(coord_x, coord_y, ship_length, direction)
-        self._draw_border(coord_x, coord_y, ship_length, direction)
-
-    def pprint(self):
-        print()
+    def __str__(self):
+        out = 'Field (max_x={}; max_y={})'.format(self.max_x, self.may_y)
         for row in self._field:
+            out += '\n\t'
             for cell in row:
-                print('{0.x:5}: {0.y} == {0.value:2}'.format(cell), end='')
-            print()
+                out += f'{cell.value:4}'
+        return out + '\n'
 
-    def get_suitable_cells(self, ship_length):
-        """
-        :param ship_length:
-        :return: List of tuples [(x, y, <direction>), ..] or empty list
-        """
-        out = []
-        for cell in chain(*self._field):
-            if cell.value == Cell.EMPTY:
-                if self._is_cell_suitable(cell, ship_length, SeaField.HORIZONTAL):
-                    out.append((cell.x, cell.y, SeaField.HORIZONTAL))
-                if self._is_cell_suitable(cell, ship_length, SeaField.VERTICAL):
-                    out.append((cell.x, cell.y, SeaField.VERTICAL))
-        return out
 
-    def _is_cell_suitable(self, cell, ship_length, direction):
-        try:
-            for cell in self._generate_cells(cell.x, cell.y, ship_length, direction):
-                if cell.value != Cell.EMPTY:
-                    return False
-        except ShipCouldNotBePlaced:
-            return False
-        return True
+class SeaPlayground:
 
-    def _generate_next_coord(self, coord_x, coord_y, direction):
-        if direction == SeaField.VERTICAL:
-            return coord_x, coord_y + 1
-        return coord_x + 1, coord_y
+    def __init__(self, max_x, max_y):
+        self._field = Field(max_x, max_y)
 
-    def _generate_cells(self, coord_x, coord_y, ship_length, direction, strict=True):
-        nx, ny = coord_x, coord_y
-        for i in range(ship_length):
-            if not self._correct_coord(nx, ny):
-                if strict:
-                    raise ShipCouldNotBePlaced()
-            else:
-                yield self._field[ny][nx]
-            nx, ny = self._generate_next_coord(nx, ny, direction)
+    def __str__(self):
+        return '<SeaPlayground>:\n' + str(self._field)
+
+    @staticmethod
+    def _next_cell(coord_x, coord_y, step, is_vertical=False):
+        return coord_x + step * (not is_vertical), coord_y + step * is_vertical
+
+    @property
+    def cells(self):
+        return self._field._cells
+
+    def set_ship(self, coord_x, coord_y, length, is_vertical=False):
+        next_cell = partial(SeaPlayground._next_cell, coord_x, coord_y, is_vertical=is_vertical)
+        [self._field.set(value=Cell.SHIP, *next_cell(i)) for i in range(length)]
--- test_seafield.py
+++ test_seafield.py
@@ -1,131 +1,25 @@
 import unittest
-from itertools import chain
-
-from seaplayground import SeaField, Cell, ShipCouldNotBePlaced
+from seaplayground import SeaPlayground, Cell
 
 
 class SeaFieldTest(unittest.TestCase):
 
-    def _check_cells(self, ff, conditions, default_value=Cell.EMPTY):
-        for cell in chain(*ff._field):
-            # print (f'{cell.x}: {cell.y} = {cell.value}', end = ' <=> ')
-            for condition, value in conditions.items():
-                if condition(cell):
-                    # print(value)
-                    self.assertEqual(cell.value, value, f'({cell.x}; {cell.y}) = {cell.value} != {value}')
-                    break
+    # create seafild
+    # add ship to coordinates
+    # add border for ship
+    # find out suitable place for ship
+    # generate ships on the map
+    # shoot
+
+    def test_set_ship(self):
+        base = SeaPlayground(5, 5)
+        base.set_ship(1, 1, 3)
+        ship = [(1, 1), (2, 1), (3, 1)]
+        print(base)
+        for cell in base.cells:
+            if (cell.x, cell.y) in ship:
+                assert cell.value == Cell.SHIP
             else:
-                #print(default_value)
-                self.assertEqual(cell.value, default_value, f'{cell.x} : {cell.y} = {cell.value} != {default_value}')
+                assert cell.value == Cell.EMPTY
 
-    def test_mark_cell(self):
-        ff = SeaField(5, 5)
-        self.assertEqual(ff.mark_cell(2, 2, 10), True)
-        conditions = {lambda cell: cell.x == 2 and cell.y == 2: 10}
-        self._check_cells(ff, conditions)
-
-    def test_mark_outrange(self):
-        ff = SeaField(5, 5)
-        self.assertEqual(ff.mark_cell(21, 2, 10), False)
-        self._check_cells(ff, dict())
-
-    def test_draw_ship(self):
-        ff = SeaField(5, 5)
-        self.assertEqual(ff._draw_ship(1, 2, 4, SeaField.HORIZONTAL), True)
-        conditions = {lambda cell: cell.y == 2 and cell.x in [1,2,3,4]: Cell.SHIP}
-        self._check_cells(ff, conditions)
-
-    def test_fail_draw_ship(self):
-        ff = SeaField(5, 5)
-        with self.assertRaises(ShipCouldNotBePlaced):
-            ff._draw_ship(1, 2, 10, SeaField.HORIZONTAL)
-        with self.assertRaises(ShipCouldNotBePlaced):
-            ff._draw_ship(4, 2, 5, SeaField.HORIZONTAL)
-        with self.assertRaises(ShipCouldNotBePlaced):
-            ff._draw_ship(14, 2, 5, SeaField.HORIZONTAL)
-
-    def test_draw_border_one_cell(self):
-        ff = SeaField(5, 5)
-        ff._draw_border(2, 2, 1)
-
-        conditions = {
-            lambda cell: cell.y in (1, 3) and cell.x in (1, 2, 3): Cell.BORDER,
-            lambda cell: cell.y == 2 and cell.x in (1, 3): Cell.BORDER
-        }
-        self._check_cells(ff, conditions)
-
-    def test_draw_several_borders_horizont(self):
-        ff = SeaField(5, 5)
-        ff._draw_border(1, 1, 3)
-        ff._draw_border(2, 3, 3)
-        conditions = {
-            lambda cell: cell.y in (0, 2) and cell.x in (0, 1, 2, 3, 4): Cell.BORDER,
-            lambda cell: cell.y == 1 and cell.x in (0, 4): Cell.BORDER,
-            lambda cell: cell.y == 4 and cell.x in (1, 2, 3, 4): Cell.BORDER,
-            lambda cell: cell.y == 3 and cell.x == 1: Cell.BORDER
-        }
-        self._check_cells(ff, conditions)
-
-    def test_draw_border_vertical(self):
-        ff = SeaField(5, 5)
-        ff._draw_border(1, 1, 3, SeaField.VERTICAL)
-        conditions = {
-            lambda cell: cell.x in (0, 2) and cell.y in (0, 1, 2, 3, 4): Cell.BORDER,
-            lambda cell: cell.x == 1 and cell.y in (0, 4): Cell.BORDER,
-        }
-        self._check_cells(ff, conditions)
-
-    def test_set_ship_horizontal(self):
-        ff = SeaField(5, 5)
-        ff.set_ship(2, 2, 3)
-        conditions = {
-            lambda cell: cell.y in (1, 3) and cell.x in (1, 2, 3, 4): Cell.BORDER,
-            lambda cell: cell.y == 2 and cell.x == 1: Cell.BORDER,
-            lambda cell: cell.y == 2 and cell.x in (2, 3, 4): Cell.SHIP
-        }
-        self._check_cells(ff, conditions)
-
-    def test_set_ship_vertical(self):
-        ff = SeaField(5, 5)
-        ff.set_ship(2, 2, 3, SeaField.VERTICAL)
-        conditions = {
-            lambda cell: cell.x in (1, 3) and cell.y in (1, 2, 3, 4): Cell.BORDER,
-            lambda cell: cell.x == 2 and cell.y == 1: Cell.BORDER,
-            lambda cell: cell.x == 2 and cell.y in (2, 3, 4): Cell.SHIP
-        }
-        self._check_cells(ff, conditions)
-
-    def test_get_suitable_cells_none(self):
-        ff = SeaField(5, 5)
-        ff.set_ship(0, 1, 1)
-        ff.set_ship(1, 3, 4)
-        suitable_cells = ff.get_suitable_cells(4)
-        self.assertEqual(len(suitable_cells), 0)
-
-    def test_get_suitable_cells_success(self):
-        ff = SeaField(5, 5)
-        ff.set_ship(0, 1, 1)
-        ff.set_ship(1, 3, 4)
-        suitable_cells = ff.get_suitable_cells(3)
-        self.assertEqual(len(suitable_cells), 2)
-        for cell in suitable_cells:
-            self.assertTrue(cell in [(2, 0, SeaField.HORIZONTAL), (2, 1, SeaField.HORIZONTAL)])
-
-    def test_get_suitable_cells_vertial_success(self):
-        ff = SeaField(5, 5)
-        ff.set_ship(0, 1, 1)
-        ff.set_ship(1, 3, 4)
-        suitable_cells = ff.get_suitable_cells(2)
-        waited_cells = [
-            (2, 0, SeaField.HORIZONTAL),
-            (3, 0, SeaField.HORIZONTAL),
-            (2, 1, SeaField.HORIZONTAL),
-            (3, 1, SeaField.HORIZONTAL),
-            (2, 0, SeaField.VERTICAL),
-            (3, 0, SeaField.VERTICAL),
-            (4, 0, SeaField.VERTICAL)]
-
-        self.assertEqual(len(suitable_cells), len(waited_cells))
-        for cell in suitable_cells:
-            self.assertTrue(cell in waited_cells)
 
