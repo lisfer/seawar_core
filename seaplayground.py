7f68ae3e2b2cffb8f3a1510b5a7de28a29012dae
lisfer <makrushinds@gmail.com>
2018-05-24 16:55:21 +0300

    check if coordinates are suitable for placing ship

--- seaplayground.py
+++ seaplayground.py
@@ -20,13 +20,10 @@ class Cell:
 class Field:
 
     def __init__(self, max_x, max_y):
-        self.max_x = max_x,
+        self.max_x = max_x
         self.may_y = max_y
         self._field = [[Cell(i, j) for i in range(max_x)] for j in range(max_y)]
         self._cells = list(chain(*self._field))
-
-    def set(self, coord_x, coord_y, value):
-        self._field[coord_y][coord_x].value = value
 
     def __str__(self):
         out = 'Field (max_x={}; max_y={})'.format(self.max_x, self.may_y)
@@ -35,6 +32,19 @@ class Field:
             for cell in row:
                 out += f'{cell.value:4}'
         return out + '\n'
+
+    @staticmethod
+    def _next_cell(coord_x, coord_y, step, is_vertical=False):
+        return coord_x + step * (not is_vertical), coord_y + step * is_vertical
+
+    def set(self, coord_x, coord_y, value):
+        self._field[coord_y][coord_x].value = value
+
+    def get(self, coord_x, coord_y):
+        return self._field[coord_y][coord_x].value
+
+    def is_cell_correct(self, coord_x, coord_y):
+        return (0 <= coord_x < self.max_x) and (0 <= coord_y < self.may_y)
 
 
 class SeaPlayground:
@@ -45,25 +55,30 @@ class SeaPlayground:
     def __str__(self):
         return '<SeaPlayground>:\n' + str(self._field)
 
-    @staticmethod
-    def _next_cell(coord_x, coord_y, step, is_vertical=False):
-        return coord_x + step * (not is_vertical), coord_y + step * is_vertical
-
     @property
     def cells(self):
         return self._field._cells
 
     def set_ship(self, coord_x, coord_y, length, is_vertical=False):
-        next_cell = partial(SeaPlayground._next_cell, coord_x, coord_y, is_vertical=is_vertical)
+        next_cell = partial(Field._next_cell, coord_x, coord_y, is_vertical=is_vertical)
         [self._field.set(value=Cell.SHIP, *next_cell(i)) for i in range(length)]
 
     def set_border(self, coord_x, coord_y, length, is_vertical=False):
         v_length, h_length = (length, 1) if is_vertical else (1, length)
         cells = []
         for i in range(v_length + 2):
-            cells.append(SeaPlayground._next_cell(coord_x - 1, coord_y - 1, i, True))
-            cells.append(SeaPlayground._next_cell(coord_x + h_length, coord_y - 1, i, True))
+            cells.append(Field._next_cell(coord_x - 1, coord_y - 1, i, True))
+            cells.append(Field._next_cell(coord_x + h_length, coord_y - 1, i, True))
         for i in range(h_length):
-            cells.append(SeaPlayground._next_cell(coord_x, coord_y - 1, i, False))
-            cells.append(SeaPlayground._next_cell(coord_x, coord_y + v_length, i, False))
+            cells.append(Field._next_cell(coord_x, coord_y - 1, i, False))
+            cells.append(Field._next_cell(coord_x, coord_y + v_length, i, False))
         [self._field.set(value=Cell.BORDER, *cell) for cell in cells]
+
+    def put_ship(self, coord_x, coord_y, length, is_vertical=False):
+        self.set_ship(coord_x, coord_y, length, is_vertical)
+        self.set_border(coord_x, coord_y, length, is_vertical)
+
+    def is_cell_suitable(self, coord_x, coord_y, length, is_vertical=False):
+        next_cell = partial(Field._next_cell, coord_x, coord_y, is_vertical=is_vertical)
+        check = lambda x, y: self._field.is_cell_correct(x, y) and self._field.get(x, y) is Cell.EMPTY
+        return all([check(*next_cell(i)) for i in range(length)])
--- test_seafield.py
+++ test_seafield.py
@@ -31,3 +31,34 @@ class SeaFieldTest(unittest.TestCase):
                 assert cell.value == Cell.BORDER
             else:
                 assert cell.value == Cell.EMPTY
+
+    def test_put_ship(self):
+        base = SeaPlayground(5, 5)
+        base.put_ship(2, 1, 3, True)
+        ship = [(2, 1), (2, 2), (2, 3)]
+        border = [(1, 0), (1, 1), (1, 2), (1, 3), (1, 4),
+                  (2, 0), (2, 4),
+                  (3, 0), (3, 1), (3, 2), (3, 3), (3, 4)]
+
+        for cell in base.cells:
+            if (cell.x, cell.y) in ship:
+                assert cell.value == Cell.SHIP
+            elif (cell.x, cell.y) in border:
+                assert cell.value == Cell.BORDER
+            else:
+                assert cell.value == Cell.EMPTY
+
+    def test_suitable_cell(self):
+        base = SeaPlayground(5, 5)
+        assert base.is_cell_suitable(1, 1, 1)
+        assert base.is_cell_suitable(1, 1, 3)
+        assert base.is_cell_suitable(1, 1, 3, True)
+
+        assert not base.is_cell_suitable(-1, 1, 1)
+        assert not base.is_cell_suitable(5, 1, 1)
+        assert not base.is_cell_suitable(1, 1, 11)
+
+        base.put_ship(1, 1, 3)
+
+        assert not base.is_cell_suitable(0, 0, 1)
+        assert not base.is_cell_suitable(0, 2, 2, True)
