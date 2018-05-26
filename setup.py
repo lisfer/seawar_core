74da44e9b12dd6ccf6c0d2f64302476335eb7541
lisfer <makrushinds@gmail.com>
2018-05-26 13:37:44 +0300

    bumpversion added

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='seawar_skeleton',
-    version='1.1',
+    version='1.1.0',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
 )
