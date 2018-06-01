7a0a5844dd36645edd85bfd2c0b39968974d9f2e
lisfer <makrushinds@gmail.com>
2018-06-01 09:29:49 +0300

    updated __init__ file

--- seawar_skeleton/__init__.py
+++ seawar_skeleton/__init__.py
@@ -1,2 +1,1 @@
-from .seaplayground import STANDARD_SHIP_FLEET, SIGNALS, IncorrectShipPosition, NoSpaceLeft, IncorrectCoordinate, \
-    Cell, Matrix, SeaField, SeaPlayground, ComputerPlayer
+from .main import *
